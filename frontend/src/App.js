e61f7cda993ef241049edef9c1318f658c3d0b2b
Emily Gavrilenko <egavrile@calpoly.edu>
2021-01-17 10:18:12 -0800

    Combine all changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import Login from './components/Login'
-<<<<<<< HEAD
-
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
-=======
 import Signup from './components/Signup'
->>>>>>> 9a2670b7403c10ce9c6f5104f8b6c8552cb74c20
 
 import './css/App.css';
 
@@ -21,13 +15,8 @@ class App extends Component {
       <Router>
           <NavBar/>
           <Switch>
-<<<<<<< HEAD
-              <PublicRoute path="/login"><Login/></PublicRoute>
-              <PrivateRoute path="/signup"><Login/></PrivateRoute>
-=======
               <Route path="/login"><Login/></Route>
               <Route path="/signup"><Signup/></Route>
->>>>>>> 9a2670b7403c10ce9c6f5104f8b6c8552cb74c20
               <Route><Home/></Route>
           </Switch>
       </Router>
