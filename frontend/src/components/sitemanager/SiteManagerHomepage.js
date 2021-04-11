1883507b8343195e180580f05298959a1c388d32
Anushree Parmar <anuaparmar@gmail.com>
2021-04-11 14:26:39 -0700

    PDF is now printing out, but working on CSS

--- frontend/src/components/sitemanager/SiteManagerHomepage.js
+++ frontend/src/components/sitemanager/SiteManagerHomepage.js
@@ -7,6 +7,9 @@ import Spinner from "react-bootstrap/Spinner"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/manager.css'
 
+import * as html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
+
 class SiteManagerHomepage extends Component {
     constructor(props) {
         super(props);
@@ -35,15 +38,34 @@ class SiteManagerHomepage extends Component {
         this.setState({totals: data.meals, routes: data.routes})
     }
 
+    printDocument() {
+        const input = document.getElementById('site-manager-container');
+    
+        html2canvas(input)
+        .then((canvas) => {
+            console.log('here')
+            const imgData = canvas.toDataURL('image/png');
+            console.log(imgData)
+            const pdf = new jsPDF();
+            pdf.addImage(canvas, 'JPEG', 2, 10, 180, 200);
+            pdf.output('dataurlnewwindow');
+            pdf.save("download.pdf");
+        })
+        ;
+    }
+
     render() {
         let {totals, routes} = this.state
         console.log(this.state.totals)
         return (
             <div className="site-manager-page">
                 <h1 className="site-manager-page-header">Site Manager Overview</h1>
+                <div className="mb5">
+                    <button onClick={this.printDocument}>Print</button>
+                </div>
                 <div>
                     <RoutesNavbar routes={this.state.routes}/>
-                    <div className="site-manager-container">
+                    <div className="site-manager-container" id="site-manager-container">
                         {this.state.totals.length >= 10 ? <MealTotals data={totals} routes={routes}/> : 
                         <div>
                             <Spinner animation="border" role="status" />
