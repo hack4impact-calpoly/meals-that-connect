ab171d51ed674d1b84c5f2420a5607a8f732c752
Emily Gavrilenko <egavrile@calpoly.edu>
2021-04-29 10:19:41 -0700

    Update schedule schema

--- backend/models/schedule.js
+++ backend/models/schedule.js
@@ -4,33 +4,10 @@ const {schedulesConnection} = require('../connections');
 const scheduleSchema = new mongoose.Schema({
     site: {type: String, required: true},
     startDate: {type: Date, required: true},
-    orders: {
-        M:{
-            volunteers: {type: Array, required: true, default: [] },
-            mealPrep: {type: Array, required: true, default: [] },
-            staff: {type: Number, required: true, default: 0},
-            computer: {type: Number, required: true, default: 0} },
-        T:{
-            volunteers: {type: Array, required: true, default: [] },
-            mealPrep: {type: Array, required: true, default: [] },
-            staff: {type: Number, required: true, default: 0} ,
-            computer: {type: Number, required: true, default: 0} },
-        W:{
-            volunteers: {type: Array, required: true, default: [] },
-            mealPrep: {type: Array, required: true, default: [] },
-            staff: {type: Number, required: true, default: 0} ,
-            computer: {type: Number, required: true, default: 0} },
-        Th:{
-            volunteers: {type: Array, required: true, default: [] },
-            mealPrep: {type: Array, required: true, default: [] },
-            staff: {type: Number, required: true, default: 0} ,
-            computer: {type: Number, required: true, default: 0} },
-        F:{
-            volunteers: {type: Array, required: true, default: [] },
-            mealPrep: {type: Array, required: true, default: [] },
-            staff: {type: Number, required: true, default: 0} ,
-            computer: {type: Number, required: true, default: 0} },
-        },
+    routes: {type: mongoose.Schema.Types.Mixed, required: true},
+    mealPrep: {type: Array, required: true, default: []},
+    staff: {type: Array, required: true, default: []},
+    computer: {type: Array, required: true, default: []},
 })
 
 const Schedule = schedulesConnection.model("schedules", scheduleSchema, "schedules")
--- backend/src/schedules.js
+++ backend/src/schedules.js
@@ -7,41 +7,72 @@ const Volunteer = require('../models/Volunteer')
 const Hours = require("../models/Hours")
 const Client = require("../models/clients")
 
-// router.post('/add', async (req, res) => {
-//   const {firstName, lastName, email, password, driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, notes, digitalSystem, completedOrientation} = req.body  
-//   var volunteerID = getID();
-//   Volunteer.findOne({'email': email}).then(function(result) {
-//   if (result) {
-//     console.log("email already in use")
-//      res.sstatus(404).send("email already in use")     
-//   }
-//   var volun = new Volunteer({volunteerID, firstName, lastName, email, password, driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, notes, digitalSystem, completedOrientation})
-//   volun.save()
-//   console.log("succcessfully added volunteer")
-//   res.status(200).send("success")
-//   }).catch(err => {
-//     console.log(err)
-//     res.send(500).send("Internal server error")
-//   })
-// })
-
-router.post('/new', async (req, res) => {
-    const {site, startDate} = req.body
-    console.log(new Date())
-    Schedule.findOne({'site': site, 'startDate': startDate}).then(function(result) {
-    if (result) {
-      console.log("email already in use")
-       res.sstatus(404).send("email already in use")     
-    }
-    var volun = new Volunteer({volunteerID, firstName, lastName, email, password, driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, notes, digitalSystem, completedOrientation})
-    volun.save()
-    console.log("succcessfully added volunteer")
-    res.status(200).send("success")
+router.post('/update', async (req, res) => {
+    let {site, startDate, routes, mealPrep, staff, computer} = req.body
+    Schedule.updateOne({'site': site, 'startDate': startDate}, {site, startDate, routes, mealPrep, staff, computer}).then(function(schedule) {
+        if (!schedule) {
+            res.status(404).send("Error in updating schedule");
+        } else {
+            console.log("schedule updated")
+            res.status(200).send("schedule updated") 
+        }
     }).catch(err => {
-      console.log(err)
-      res.send(500).send("Internal server error")
+        console.log(err)
+        res.send(500).send("Error updating schedule")
     })
-  })
+})
+
+
+router.post('/get', async (req, res) => {
+    let {site, startDate} = req.body
+    Schedule.findOne({'site': site, 'startDate': startDate}).then(async function(result) {
+        if (result) {
+            existing_routes = result.routes
+            getRoutes(site).then((route_names) => {
+                let updated = false
+                route_names.forEach(route => {
+                    if (!(route in existing_routes)){
+                        updated = true
+                        existing_routes[route] = [ [], [], [], [], [] ]
+                    }
+                })  
+                if (updated) {
+                    Schedule.updateOne({'_id': result._id}, {'routes': existing_routes}).then(function(schedule) {
+                        if (!schedule) {
+                            res.send("Error in updating schedule");
+                        } else {
+                            console.log("schedule updated")
+                            res.status(200).send(result) 
+                        }
+                    })
+                }   
+                else {
+                    res.status(200).send(result) 
+                    console.log("schedule up to date")
+                }
+            })   
+        }
+        else {
+            getRoutes(site).then((route_names) => {
+                let routes = {}
+                route_names.forEach(route => {
+                    let data = [ [], [], [], [], [] ]
+                    routes[route] = data
+                })       
+                mealPrep = [ [], [], [], [], [] ]
+                staff = [ [], [], [], [], [] ]
+                computer = [ [], [], [], [], [] ]
+                var new_schedule = Schedule({site, startDate, routes, mealPrep, staff, computer})
+                new_schedule.save()
+                console.log("Schedule successfully created")
+                res.status(200).send(new_schedule)
+            })
+        }
+    }).catch(err => {
+        console.log(err)
+        res.send(500).send("Error adding schedule")
+    })
+})
 
 async function getRoutes(site) {
     let routes = {}
@@ -60,30 +91,5 @@ async function getRoutes(site) {
     })
     return Object.keys(routes)
 }
-
-router.post('/get', async (req, res) => {
-  const {site, startDate} = req.body
-  Schedule.find({site: site, startDate: startDate}, function (err, schedule) {
-    if (err) {
-        console.log("Error fetching schedule")
-        res.send(500).send("Internal server error")
-    }
-    else {
-        console.log(schedule)
-        res.status(200).send(schedule)
-    }
-  })
-})
-
-// router.get('/update', async (req, res) => {
-//   Volunteer.find({}, function (err, volunteer) {
-//     if (err) {
-//       console.log(err)
-//     }
-//     else {
-//       res.send(volunteer)
-//     }
-//   })
-// })
 
 module.exports = router;
