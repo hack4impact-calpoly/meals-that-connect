b62a9ccafd1eec8572b8b0f0e13cc01f5b0d8449
Emily Gavrilenko <egavrile@calpoly.edu>
2021-04-29 20:29:33 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require("cors");
-require('dotenv').config()
-
 
 const app = express();
 app.use(cors());
