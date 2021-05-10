d18938b1de8a696e88fbc9d29b977ccc1e8e0703
Emily Gavrilenko <egavrile@calpoly.edu>
2021-05-09 21:30:39 -0700

    add scheduling functionality

--- frontend/src/components/sitemanager/volunteers/VolunteerSchedule.js
+++ frontend/src/components/sitemanager/volunteers/VolunteerSchedule.js
@@ -46,32 +46,54 @@ class VolunteerSchedule extends Component {
     async componentWillMount(){
         this.state.weekArr[1] = this.getMonday(new Date());
         console.log("mount")
+        await this.fetchVolunteers();
         this.fetchSchedule();
     }
+
+
+    async fetchVolunteers(){
+        
+        let info = {
+            site: localStorage.getItem("site"),
+        }
+        let response = await fetch(process.env.REACT_APP_SERVER_URL + 'volunteers/availability', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+        const data = await response.json();
+        console.log("data")
+        console.log(data)
+        
+        this.setState({volunteers: data})
+    }
+
 
     async fetchSchedule(){
         
         let info = {
-        site: localStorage.getItem("site"),
-        startDate: this.state.weekArr[1] //new Date("May 3, 2021"), // HEELPPPP: task --- figureout how to get start date!
-    }
-    let response = await fetch(process.env.REACT_APP_SERVER_URL + 'schedules/get', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(info)
-    })
-    const data = await response.json();
-    console.log("data")
-    console.log(data)
-    
-    this.setState({loaded: true, routes: data.routes, mealPrep: data.mealPrep, mealPrep2: data.mealPrep2, mealPrep3: data.mealPrep3, mealPrep4: data.mealPrep4, mealPrep5: data.mealPrep5, staff: data.staff, computer: data.computer})
+            site: localStorage.getItem("site"),
+            startDate: this.state.weekArr[1] 
+        }
+        let response = await fetch(process.env.REACT_APP_SERVER_URL + 'schedules/get', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(info)
+        })
+        const data = await response.json();
+        console.log("data")
+        console.log(data)
+        
+        this.setState({loaded: true, routes: data.routes, mealPrep: data.mealPrep, mealPrep2: data.mealPrep2, mealPrep3: data.mealPrep3, mealPrep4: data.mealPrep4, mealPrep5: data.mealPrep5, staff: data.staff, computer: data.computer})
     }
 
     render() {
         console.log("schedule overview week array and routes")
-        let {loaded, routes, weekArr, holidayArr, mealPrep, mealPrep2, mealPrep3, mealPrep4, mealPrep5, staff, computer} = this.state
+        let {loaded, routes, weekArr, holidayArr, mealPrep, mealPrep2, mealPrep3, mealPrep4, mealPrep5, staff, computer, volunteers} = this.state
         //console.log(weekArr[1])
         //console.log(routes[4])
         return (
@@ -79,7 +101,7 @@ class VolunteerSchedule extends Component {
                 <h1 className="site-manager-page-header">Volunteer Schedule Overview</h1>
                 <VolunteerNavbar updateWeek={this.updateWeek} updateHoliday={this.updateHoliday}/>
                 <div className="site-manager-container" style={{paddingLeft: 0}}>
-                {this.state.loaded ? <VolunteersScheduleTable routes={routes} weekArr={weekArr} holidayArr={holidayArr} mealPrep={mealPrep} mealPrep2={mealPrep2} mealPrep3={mealPrep3} mealPrep4={mealPrep4} mealPrep5={mealPrep5} staff={staff} computer={computer}/> :
+                {this.state.loaded ? <VolunteersScheduleTable volunteers={volunteers} routes={routes} weekArr={weekArr} holidayArr={holidayArr} mealPrep={mealPrep} mealPrep2={mealPrep2} mealPrep3={mealPrep3} mealPrep4={mealPrep4} mealPrep5={mealPrep5} staff={staff} computer={computer}/> :
                     <div>
                         <Spinner animation="border" role="status" />
                     </div>}
