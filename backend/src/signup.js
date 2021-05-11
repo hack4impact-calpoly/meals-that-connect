face9c69b2de40a172919329444cf96e970e1547
Emily Gavrilenko <egavrile@calpoly.edu>
2021-05-11 10:15:56 -0700

    Work on admin signup bug

--- backend/src/signup.js
+++ backend/src/signup.js
@@ -67,69 +67,127 @@ router.post('/', async (req, res) =>{
     })
  });
 
+async function newAdminUser(data, password, userType) {
+   let { firstName, lastName, email, isAuthenticated, site} = data
+   let output = false
+
+   await userType.findOne({'email': email}).then(function(result) {
+      if (result) {
+         console.log("email already in use")
+         return false
+      } 
+      else {
+         var doc;
+         if (data.user == "volunteer") {
+            var volunteerID = getID();
+            const {driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, phoneNumber, availability} = data 
+            doc = new userType({ volunteerID, firstName, lastName, email, password, driver, kitchenStaff, 
+                    isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, admin: true })
+         } else {
+            doc = new userType({ firstName, lastName, email, password, isAuthenticated, site, admin: true })
+         }
+         doc.save()
+         output = true
+         return true
+      }
+   }).catch(err => {
+      console.log(err)
+      return false
+   })
+   return output
+ }
+
  router.post('/admin', async (req, res) =>{
-   const {firstName, lastName, email, isAuthenticated, site, user, code} = req.body
+   const { code } = req.body
    const password = bcrypt.hashSync(req.body.password, 9);
    if (code !== process.env.ADMIN_CODE) {
       res.status(404).send("Invalid code")
       console.log("Invalid code")
       return;
    }
+
+   let created = await newAdminUser(req.body, password, Volunteer)
+   if (!created) { 
+      res.status(404).send("email already in use")
+      return
+   }
+   created = await newAdminUser(req.body, password, SiteManager)
+   if (!created) { 
+      res.status(404).send("email already in use")
+      return
+   }
+   created = await newAdminUser(req.body, password, DataEntry)
+   if (!created) { 
+      res.status(404).send("email already in use")
+      return
+   }
+
+   res.status(200).send("Successfully added all user types")
+})
  
-   await Volunteer.findOne({'email': email}).then(function(result) {
-      if (result) {
-         console.log("email already in use")
-         res.status(404).send("email already in use")
-         return;
-      } 
-      else {
-         var volunteerID = getID();
-         const {driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, phoneNumber, availability} = req.body  
-         var doc = new Volunteer({ volunteerID, firstName, lastName, email, password, driver, kitchenStaff, 
-            isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, admin: true })
-         doc.save()
-         console.log("successfully added volunteer")
-      }
-   }).catch(err => {
-      console.log(err)
-      res.send(500).send("Internal server error")
-      return;
-   })
+//    await Volunteer.findOne({'email': email}).then(function(result) {
+//       if (result) {
+//          console.log("email already in use")
+//          res.status(404).send("email already in use")
+//          return;
+//       } 
+//       else {
+//          var volunteerID = getID();
+//          const {driver, kitchenStaff, isAuthenticated_driver, isAuthenticated_kitchenStaff, phoneNumber, availability} = req.body  
+//          var doc = new Volunteer({ volunteerID, firstName, lastName, email, password, driver, kitchenStaff, 
+//             isAuthenticated_driver, isAuthenticated_kitchenStaff, site, phoneNumber, availability, admin: true })
+//          doc.save().then(result => {
+//             output = output + " successfully added volunteer"
+//             console.log(output)
+//          })
+//          console.log("successfully added volunteer")
+//       }
+//    }).catch(err => {
+//       console.log(err)
+//       res.send(500).send("Internal server error")
+//       return;
+//    })
 
-   await SiteManager.findOne({'email': email}).then(function(result) {
-      if (result) {
-         console.log("email already in use")
-         res.status(404).send("email already in use")
-         return;
-      } 
-      else {
-         var doc = new SiteManager({ firstName, lastName, email, password, isAuthenticated, site, admin: true })
-         doc.save().then(result => {console.log(result)})
-         console.log("successfully added site manager")
-      }
-   }).catch(err => {
-      console.log(err)
-      res.send(500).send("Internal server error")
-      return;
-   })
+//    await SiteManager.findOne({'email': email}).then(function(result) {
+//       if (result) {
+//          console.log("email already in use")
+//          res.status(404).send("email already in use")
+//          return;
+//       } 
+//       else {
+//          var doc = new SiteManager({ firstName, lastName, email, password, isAuthenticated, site, admin: true })
+//          doc.save().then(result => {
+//             output = output + " successfully added site manager"
+//             console.log(output)
+//          })
+//          console.log("successfully added site manager")
+//       }
+//    }).catch(err => {
+//       console.log(err)
+//       res.send(500).send("Internal server error")
+//       return;
+//    })
 
-   DataEntry.findOne({'email': email}).then(function(result) {
-      if (result) {
-         console.log("email already in use")
-         res.status(404).send("email already in use")
-         return;
-      } 
-      else {
-         var doc = new DataEntry({ firstName, lastName, email, password, isAuthenticated, site, admin: true })
-         doc.save().then(result => {console.log(result)})
-         console.log("successfully added data entry")
-         res.status(200).send("success")
-      }
-   }).catch(err => {
-      console.log(err)
-      res.send(500).send("Internal server error")
-   })
-});
+//    await DataEntry.findOne({'email': email}).then(function(result) {
+//       if (result) {
+//          console.log("email already in use")
+//          res.status(404).send("email already in use")
+//          return;
+//       } 
+//       else {
+//          var doc = new DataEntry({ firstName, lastName, email, password, isAuthenticated, site, admin: true })
+//          doc.save().then(result => {
+//             output = output + " successfully added data entry"
+//             console.log(output)
+//          })
+//          console.log("successfully added data entry")
+//          res.status(200).send(output)
+//       }
+//    }).catch(err => {
+//       console.log(err)
+//       res.send(500).send("Internal server error")
+//    })
+// });
 
 // Generates random string ID. Very low probability of duplicate IDs
 function getID() {
