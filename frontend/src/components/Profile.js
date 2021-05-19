89c3a075d55dcfa5f2a375e705c0136b08aff215
MixelPixel <hthemaster@gmail.com>
2021-05-18 17:20:42 -0700

    Made User Type uneditable unless editing profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -226,6 +226,7 @@ class Profile extends Component {
                     <div style={{width: 300, marginLeft: userType === "volunteer" ? 130 : 0}}>
                         <p className='input-firstName'>User Type</p>
                         <Select 
+                            isDisabled={this.state.readOnly}
                             options={options} 
                             placeholder="User Type" 
                             styles={customStyles}
